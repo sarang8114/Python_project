75c90a3f6526d4b16de7f33a95babf03aca880cd
Mohit_vaidya <mohiiitvaidya@gmail.com>
2024-03-06 19:41:33 +0530

    add contact and email attribute to Donor

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ django.setup()
 from organ_donation_app.models import Donor
 
 
-new_donor = Donor(name='Sarang', age=20, blood_group='O+', organ_to_donate='Eyes')
+new_donor = Donor(name='Sarang', age=20, blood_group='O+', organ_to_donate='Eyes',contact_number='9879879876',email='sarang@gmail.com')
 new_donor.save()
--- organ_donation_app/models.py
+++ organ_donation_app/models.py
@@ -7,7 +7,9 @@ class Donor(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     blood_group = models.CharField(max_length=10)
-    organ_to_donate = models.CharField(max_length=100) 
+    organ_to_donate = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15, default='1234567890')  # Default contact number
+    email = models.EmailField(default='example@example.com')  
 
     def __str__(self):
         return self.name
